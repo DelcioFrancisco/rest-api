13ff4d94012201d072aa909463dc9aa3578b72bf
DelcioFrancisco <delciofrancisco_1@hotmail.com>
2025-07-09 17:05:05 +0100

    chore: setting app

---
+++ src/main/resources/db.migration/V01__new-table-opportunity.sql
@@ -0,0 +1,8 @@
+create table oportunity (
+    id biginit auto_increment not null,
+    name_company varchar(80) not null,
+    description varchar(200) not null,
+    value_company decimal(10,2),
+
+    primary key (id)
+);
