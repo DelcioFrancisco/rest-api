aab6663e9d4c252ef206945a6d67147da33ad350
DelcioFrancisco <delciofrancisco_1@hotmail.com>
2025-10-15 11:15:00 +0100

    feat: GlobalExceptionHandler

--- src/main/java/com/example/rest_api/infra/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/rest_api/infra/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,25 @@
 package com.example.rest_api.infra.exceptions;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import java.util.Map;
+
+@RestControllerAdvice // or @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ConfigDataResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFound(ConfigDataResourceNotFoundException ex) {
+        return Map.of("error", ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class) // fallback for all other exceptions
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleGeneral(Exception ex) {
+        return Map.of("error", "Unexpected error occurred");
+    }
 }
