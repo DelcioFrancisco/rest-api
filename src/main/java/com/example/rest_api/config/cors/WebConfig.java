56e38649cd282e9556bceb2ef9204d49fe43749b
DelcioFrancisco <delciofrancisco_1@hotmail.com>
2025-12-07 22:05:39 +0000

    refactor: webcobfig reorganazation

--- src/main/java/com/example/rest_api/config/cors/WebConfig.java
+++ src/main/java/com/example/rest_api/config/cors/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.rest_api.infra.cors;
+package com.example.rest_api.config.cors;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
