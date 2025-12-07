b6424623dc7497067b54e42f1de0fd39719df4b9
DelcioFrancisco <delciofrancisco_1@hotmail.com>
2025-12-07 22:07:02 +0000

    refactor: deleted files

--- src/main/java/com/example/rest_api/adapter/out/persistence/OportunityJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.rest_api.adapter.out.persistence;
-
-import com.example.rest_api.domain.model.Opportunity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OportunityJpaRepository extends JpaRepository<Opportunity, Long> {
-}
--- src/main/java/com/example/rest_api/application/TransformOportunityService.java
+++
@@ -1,4 +0,0 @@
-package com.example.rest_api.application;
-
-public class TransformOportunityService {
-}
