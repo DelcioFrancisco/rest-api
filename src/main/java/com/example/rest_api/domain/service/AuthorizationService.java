07da10d3c45ea5e9ee40f2ea6046a943b858ff3f
DelcioFrancisco <delciofrancisco_1@hotmail.com>
2026-01-22 00:33:18 +0000

    feat: add log in auth service

--- src/main/java/com/example/rest_api/domain/service/AuthorizationService.java
+++ src/main/java/com/example/rest_api/domain/service/AuthorizationService.java
@@ -1,6 +1,7 @@
 package com.example.rest_api.domain.service;
 
 import com.example.rest_api.adapter.out.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,6 +9,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class AuthorizationService implements UserDetailsService {
 
     @Autowired
@@ -15,6 +17,7 @@ public class AuthorizationService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Getting user by username " + username);
         return repository.findByLogin(username);
     }
 }
